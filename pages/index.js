de1ab0e8ce0b674b4d41ef09f8ed94da421a7942
Pragyan <pragyan52yadav@gmail.com>
2023-07-21 10:42:59 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <>
+            <div className="h-96 flex justify-center items-center text-4xl">
+                Template for NextJS + Tailwind CSS
+                
+            </div>
+        </>
+    );
+};
+
+export default Home;
